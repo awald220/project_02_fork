a46493efe37ce2a9f96a3b7c4827e2670adc9b2a
Jacob Zirbel <jacobzirbel@gmail.com>
2020-06-15 21:48:08 -0500

    start html routes

--- App/routes/htmlRoutes.js
+++ App/routes/htmlRoutes.js
@@ -11,16 +11,43 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/play',(req,res)=>{
+    let userId = req.body.userId;
+    db.Game.findAll({where: {busy: false, active: true}}).then(function (results){
+      let game;
+      if(results.length === 0){
+        // game = createGame()
+      }else{
+        game =results.find(result=>{
+          let users = result.users.split(',');
+          return !users.some(user=>user===userId)
+        })
+        if(!game){
+          // game = createGame()
+        }
+      }
+      let prompts = [game.first, game.second, game.third, game.fourth, game.final]
+      prompts = prompts.filter(e=>e)
+      res.render('gamePlay', {prompt: prompts[prompts.length-1], game: game.id})
+    })
+  })
+
   // Load example page and pass in an example by id
-  app.get('/example/:id', function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
+  app.get('/game/:id', function(req, res) {
+    db.Game.findOne({ where: { id: req.params.id } }).then(function(
+      dbGame
     ) {
-      res.render('example', {
-        example: dbExample,
+      res.render('gameDetails', {
+        game: dbGame,
       });
     });
   });
+
+  app.get('allGames', (req,res)=>{
+    db.Game.findAll({active: false}).then(result=>{
+      res.render('allGames',{games: result})
+    })
+  })
 
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
