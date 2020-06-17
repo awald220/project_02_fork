c5a1e51fde84c9876fa998d94d154f17769e05d8
ggank <13955056+kaygrog@users.noreply.github.com>
2020-06-17 17:40:25 -0500

    Add game-api-routes

---
+++ App/routes/game-api-routes.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+
+module.exports = function(app) {
+	app.get("/api/game/open", function(req, res) {
+		db.Game.findAll({
+			where: {
+				active: true,
+				busy: false,
+			},
+		}).then(function(dbGame) {
+			res.json(dbGame);
+		});
+	});
+
+	app.put("/api/game", function(req, res) {
+		db.Game.update(req.body, {
+			where: {
+				id: req.body.id,
+			},
+		}).then(function(dbGame) {
+			res.json(dbGame);
+		});
+	});
+};
