7634362e10dce2bf876fb13b5ae082162bf61bd5
Ashley Waldner <ashleywaldner22@gmail.com>
2020-06-17 19:04:29 -0500

    remove example stuff

--- App/routes/htmlRoutes.js
+++ App/routes/htmlRoutes.js
@@ -3,12 +3,9 @@ const db = require("../models");
 module.exports =  function(app) {
 	// Load index page
 	app.get("/", function(req, res) {
-		db.Example.findAll({}).then(function(dbExamples) {
 			res.render("index", {
 				msg: "Welcome!",
-				examples: dbExamples,
 			});
-		});
 	});
 
 	app.get("/play", (req, res) => {
